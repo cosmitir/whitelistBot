7c952473f90b01d2bc35c7493042a3ff20d597b2
cosmitir <idgr2001@protonmail.com>
2022-01-20 18:01:31 +0000

    fixed double logging, now the message disappears after 2.5seconds

--- main.js
+++ main.js
@@ -12,28 +12,34 @@ bot.on("ready", () => {
 var info = "";
 
 // The bot will get an API signal that a message was arrived, compare whitelist, delete any message containing web protocols || BOOM log everything
-bot.on("messageCreate", (msg) => {
+bot.on("messageCreate", async msg => {
     if (domainWhitelist.some(domain => msg.content.includes(`http://${domain}/`) || msg.content.includes(`https://${domain}/`))) {
         info = `Whitelisted || ${msg.author.id} | ${msg.author.username}#${msg.author.discriminator} || ${msg.content}`;
     } else if (msg.content.includes("http" || "https")) {
+        info = `Deleted || ${msg.author.id} | ${msg.author.username}#${msg.author.discriminator} || ${msg.content}`;
         bot.deleteMessage(msg.channel.id, msg.id, "Non whitelisted link in message!");
-        bot.createMessage(msg.channel.id, {
+        return bot.createMessage(msg.channel.id, {
             embed: {
                 title: "Warning",
                 description: `<@${msg.author.id}> Stop sending links!`,
                     color: 0x8532D9,
-                    fields: [
-                        {
+                    fields: [{
                             name: "Support",
                             value: "If you think this was a mistake open a ticket with a screenshot of the link and the reason why it should be whitelisted.",
-                    },
-                ]
+                    },]
             }
+        }).then(async a => {
+            await delay(2500);
+            return a.delete();
         });
-        info = `Deleted || ${msg.author.id} | ${msg.author.username}#${msg.author.discriminator} || ${msg.content}`;
     }
     console.log(info);
 });
+
+// Fuck it, ill delay it my self :joy:
+function delay(ms) {
+    return new Promise(r => setTimeout(r, ms));
+}
 
 // "Connect me to the world wide web so I can destroy it" - AI's 2069
 bot.connect();
