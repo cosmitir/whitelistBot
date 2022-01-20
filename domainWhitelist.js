e3b040812a64168da2d9240a985fbf4ae94c677b
cosmitir <idgr2001@protonmail.com>
2022-01-20 18:43:32 +0000

    more domains

--- domainWhitelist.js
+++ domainWhitelist.js
@@ -5,7 +5,15 @@ let domainWhitelist = [
     "discordapp.com",
     "www.discordapp.com",
     "tenor.com",
-    "www.tenor.com"
+    "www.tenor.com",
+    "twitch.tv",
+    "www.twitch.tv",
+    "youtube.com",
+    "www.youtube.com",
+    "medal.tv",
+    "www.medal.tv",
+    "streamable.com",
+    "www.streamable.com",
 ];
 
 exports.domainWhitelist = domainWhitelist;
